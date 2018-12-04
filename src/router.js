d04a94d61063c69292f586ca6ca6f1db7d793bcd
Bonnie <baoni_lai@163.com>
2018-12-04 19:46:26 +0800

    V1-201812041946

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/views/HelloWorld'
+import TestDataTable from '@/views/TestDataTable'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'TestDataTable',
+      component: TestDataTable
     }
   ]
 })
---
+++ src/views/TestDataTable.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dt-outer">
+    <data-table></data-table>
+  </div>
+</template>
+<script>
+    import DataTable from './components/DataTable.vue'
+    export default {
+      name: 'TestDataTable',
+      data () {
+        return {
+
+        }
+      },
+      components: {
+        DataTable: DataTable
+      }
+    }
+</script>
